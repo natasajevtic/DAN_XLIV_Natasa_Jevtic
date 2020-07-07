f944515ec12782ae42f59a3ca6ad889c354a2c4a
natasajevtic <nataly.jevtic.95@gmail.com>
2020-07-07 21:57:12 +0200

    Class Menu created

---
+++ DAN_XLIV_Natasa_Jevtic/Zadatak_1/Models/Menu.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+
+namespace Zadatak_1.Models
+{
+    class Menu
+    {
+        /// <summary>
+        /// This methods creates a list of data from view of menu.
+        /// </summary>
+        /// <returns></returns>
+        public List<vwMenu> GetMenu()
+        {
+            try
+            {
+                using (PizzeriaEntities context = new PizzeriaEntities())
+                {
+                    List<vwMenu> menu = new List<vwMenu>();
+                    menu = (from x in context.vwMenus select x).ToList();
+                    return menu;
+                }
+            }
+            catch (Exception ex)
+            {
+                Debug.WriteLine("Exception" + ex.Message.ToString());
+                return null;
+            }
+        }
+    }
+}
+
